6c7ff019a2bc1c4b42c8deca8fcba9ec8d87b0ee
Andrew <andrewnijmeh1@gmail.com>
2022-05-31 16:52:51 -0400

    feat: (docker compose for redis, postgres)'

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config)
 	SwaggerModule.setup("api", app, document)
 
-	await app.listen(process.env.PORT)
+	await app.listen(8080)
 }
 bootstrap()
