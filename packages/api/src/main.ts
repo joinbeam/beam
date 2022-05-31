f19088b38650b2f453ebdf8f60faa89cf7eecc12
Andrew <andrewnijmeh1@gmail.com>
2022-05-31 15:38:38 -0400

    feat: (swagger)

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core"
-import { AppModule } from "./app.module"
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
+import { AppModule } from "./modules/app.module"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+
+	const config = new DocumentBuilder()
+		.setTitle("Beam API")
+		.setDescription("Beam API written in NestJS & GraphQL.")
+		.setVersion("1.0")
+		.addTag("beam")
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup("api", app, document)
+
+	await app.listen(process.env.PORT)
 }
 bootstrap()
