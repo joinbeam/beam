7a7433580912bc9174b2be73c5ad528956b58668
Andrew <andrewnijmeh1@gmail.com>
2022-06-06 15:56:19 -0400

    i18n: (fixes)

--- packages/landing/next.config.js
+++ packages/landing/next.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	i18n: {
 		// These are all the locales you want to support in
 		// your application
-		locales: ["en-US", "fr", "nl-NL"],
+		locales: ["en-US", "fr"],
 		// This is the default locale you want to be used when visiting
 		// a non-locale prefixed path e.g. `/hello`
 		defaultLocale: "en-US",
@@ -21,14 +21,8 @@ module.exports = {
 				defaultLocale: "en-US",
 			},
 			{
-				domain: "example.nl",
-				defaultLocale: "nl-NL",
-			},
-			{
 				domain: "example.fr",
 				defaultLocale: "fr",
-				// an optional http field can also be used to test
-				// locale domains locally with http instead of https
 				http: true,
 			},
 		],
